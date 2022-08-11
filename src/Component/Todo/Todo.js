99f81dec853cfffca40549ab87e60684c5cb12d1
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-08-11 17:35:12 +0200

    Modification of checkbox system and addind custom checkbox

--- src/Component/Todo/Todo.js
+++ src/Component/Todo/Todo.js
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 import './Todo.css';
-
+import checkMark from '../../Assets/check-mark.png';
 
 export default function Todo(props) {
   const [status, setStatus] = useState(props.state);
 
   const changeStatus = () => {
     const newStatus = status === 'finished' ? 'unfinished' : 'finished';
-    setStatus(newStatus)
-    props.changeTodoState(props.id, newStatus)
+    setStatus(newStatus);
+    props.changeTodoState(props.id, newStatus);
     // props.orderTodos()
   };
 
   return (
     <div className='todo-container'>
+      {props.title}
       <label
         className={
           'todo-checkbox-label' + (status === 'finished' ? ' finished' : '')
         }
       >
-        {props.title}
         <input
           type='checkbox'
           onChange={changeStatus}
           checked={status === 'finished' ? true : false}
+          className='todo-checkbox'
         />
+        <div className='custom-checkbox'>
+          <img
+            src={checkMark}
+            alt='custom check-mark'
+            className='custom-checkmark'
+          />
+        </div>
       </label>
     </div>
   );
