763300fc05c8264727c5a14ba5bce885f40a6e76
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-08-11 17:08:41 +0200

    Change a TODO state user storie : Added logic for checked or uncheked todo and stylizing according to todo state

--- src/Component/Todo/Todo.js
+++ src/Component/Todo/Todo.js
@@ -1,15 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 
-export default function Todo(props) {
 
-  const [status, setStatus] = useState(props.state)
+export default function Todo(props) {
+  const [status, setStatus] = useState(props.state);
+
+  const changeStatus = () => {
+    const newStatus = status === 'finished' ? 'unfinished' : 'finished';
+    setStatus(newStatus)
+    props.changeTodoState(props.id, newStatus)
+    // props.orderTodos()
+  };
 
   return (
-    <div className={'todo-container'}>
-      {props.title}
-      <label className='todo-checkbox-label'>
-        <input type='checkbox' checked={status === "finished" ? true : false} />
+    <div className='todo-container'>
+      <label
+        className={
+          'todo-checkbox-label' + (status === 'finished' ? ' finished' : '')
+        }
+      >
+        {props.title}
+        <input
+          type='checkbox'
+          onChange={changeStatus}
+          checked={status === 'finished' ? true : false}
+        />
       </label>
     </div>
   );
--- src/Page/ToDoList/ToDoList.js
+++ src/Page/ToDoList/ToDoList.js
@@ -1,12 +1,12 @@
 import { React, useState, useEffect } from 'react';
 import Header from '../../Layouts/Header/Header';
 import Todo from '../../Component/Todo/Todo';
-import './ToDoList.css'
+import './ToDoList.css';
 
 export default function ToDoList() {
   const [todoList, setTodoList] = useState([]);
 
-  // Get all todo on first rendering
+  // Get all todo on first rendering from mocked server
   useEffect(() => {
     if (todoList.length === 0) {
       fetch('http://localhost:8080/todos/')
@@ -16,11 +16,38 @@ export default function ToDoList() {
         });
     }
   }, []);
-  
-  // Order todos, order first by state from unfinished to finished and then, order by date from newer to older
-  const orderTodos = (todos) => {
-    return todos.sort((a, b) => b.state.localeCompare(a.state) || b.createdAt.localeCompare(a.createdAt))
-  }
+
+  // Change todo state beetween finished and unfinished in mocked server
+  const changeTodoState = (id, newState) => {
+    fetch(`http://localhost:8080/todos/${id}`, {
+      method: 'PATCH',
+      headers: new Headers({ 'content-type': 'application/json' }),
+      body: JSON.stringify({ state: newState }),
+    }).then(() => updateStateOnTodoList(id, newState));
+  };
+
+  // Update todoList state with new checked or unchecked todo
+  const updateStateOnTodoList = (id, newState) => {
+    const newTodoList = [...todoList];
+    newTodoList.forEach((todo) => {
+      if (todo.id === id) {
+        todo.state = newState;
+      }
+    });
+    setTodoList(orderTodos(newTodoList));
+  };
+
+    // Order todos, first by state from unfinished to finished and then, by date from newer to older
+  const orderTodos = (todoToSort) => {
+    // get todoList from parameter or from todoList state
+    if (!todoToSort) {
+      todoToSort = [...todoList];
+    }
+    return todoToSort.sort(
+      (a, b) =>
+        b.state.localeCompare(a.state) || b.createdAt.localeCompare(a.createdAt)
+    );
+  };
 
   return (
     <>
@@ -29,7 +56,15 @@ export default function ToDoList() {
         <h2>Mes Todo</h2>
         {/* Show all todo */}
         {todoList.map((todo) => {
-          return <Todo title={todo.title} key={todo.id} state={todo.state}/>;
+          return (
+            <Todo
+              title={todo.title}
+              key={todo.id}
+              state={todo.state}
+              id={todo.id}
+              changeTodoState={changeTodoState}
+            />
+          );
         })}
       </main>
     </>
