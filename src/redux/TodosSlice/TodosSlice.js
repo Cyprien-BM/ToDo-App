7bc3d48ab9910d4262e1be85a270f0a0576972e0
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-08-12 09:23:58 +0200

    Redux implementation

---
+++ src/redux/TodosSlice/TodosSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {},
+});
+
+
+export default todosSlice.reducer
